c19b77a08fb1edabfc60213f55dab8cff6483f61
Chen Kang Yang <30970698+chen-ky@users.noreply.github.com>
2022-12-27 12:23:23 +1100

    Expose more httpx exception

--- src/wise_client/__init__.py
+++ src/wise_client/__init__.py
@@ -1,2 +1,2 @@
-from .wise_client import WiseClient
-from httpx import HTTPError, HTTPStatusError
+from .wise_client import WiseClient  # noqa: F401
+from httpx import HTTPError, HTTPStatusError, RequestError  # noqa: F401
